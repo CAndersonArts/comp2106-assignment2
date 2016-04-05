9e196db15909550e145c91991524110df0677d7b
CAndersonArts <connorandersonarts@gmail.com>
2016-04-05 03:16:56 -0400

    Added Facebook Login

---
+++ config/fb.js
@@ -0,0 +1,6 @@
+// facebook app settings - fb.js
+module.exports = {
+  'appID' : '1149796045032150',
+  'appSecret' : '72643507d40811ef4b1b36f6fdc3cfde',
+  'callbackUrl' : 'http://comp2106assignment2.herokuapp.com/auth/facebook/callback'
+}
